f32b2eb5cfaec18d86bb15b610cf58bbc5e6f259
VamsiNayunipati <vamsichowdary436@gmail.com>
2024-02-29 13:08:56 -0500

    plugin code

--- app/commands/menu/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.commands import Command, CommandHandler, list
-
-class MenuCommand(Command):
-    def execute(self):
-        self.command_handler = CommandHandler()
-        print("Menu",list)
